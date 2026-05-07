38c2a052d14ba3e708378bff4427c923a4967a0f
akashdeepdwivedi-xccelera <akashdeep.dwivedi@xccelera.ai>
2026-05-07 18:09:39 +0530

    Added headless Chrome support for CI

--- src/test/java/TestBase/TestBase.java
+++ src/test/java/TestBase/TestBase.java
@@ -4,6 +4,7 @@ import java.time.Duration;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 import utils.ConfigReader;
@@ -14,27 +15,36 @@ public class TestBase {
     ConfigReader config;
 
     public void initialization() {
-    	
-    	config = new ConfigReader();
+
+        config = new ConfigReader();
+
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--headless=new");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--disable-dev-shm-usage");
+
         String browser = config.getBrowser();
 
         if (browser.equalsIgnoreCase("chrome")) {
 
-            driver = new ChromeDriver();
+            driver = new ChromeDriver(options);
+
+        } else if (browser.equalsIgnoreCase("firefox")) {
+
+            driver = new FirefoxDriver();
         }
-         else if (browser.equalsIgnoreCase("firefox")) {
-             driver = new FirefoxDriver();
-         }
 
         driver.manage().window().maximize();
         driver.manage().deleteAllCookies();
 
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(config.getImplicitWait()));
+        driver.manage().timeouts()
+                .implicitlyWait(Duration.ofSeconds(config.getImplicitWait()));
 
         driver.get(config.getUrl());
     }
 
     public void tearDown() {
+
         if (driver != null) {
             driver.quit();
         }
